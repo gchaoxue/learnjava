8b05b0a00a7d809f03da5b24f5b46bcb03c60c62
gchaoxue <gchaoxue@gmail.com>
2018-03-26 23:16:49 +0800

    hello, world!

---
+++ src/main/java/tutorials/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package tutorials.helloworld;
+
+public class HelloWorld {
+    private static final String str = "Hello, world!";
+
+    public static void main(String[] args) {
+        System.out.println(str);
+    }
+}
