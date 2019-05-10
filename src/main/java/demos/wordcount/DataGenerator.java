54055ea6ba536a967b57fc13f7c96588605b1d43
gchaoxue <chaoxue.guo@iquantex.com>
2019-05-10 17:22:41 +0800

    temp: add brute-forced word count

---
+++ src/main/java/demos/wordcount/DataGenerator.java
@@ -0,0 +1,13 @@
+package demos.wordcount;
+
+/**
+ * Created by gchaoxue on 2019/5/10
+ */
+public class DataGenerator {
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/demos/wordcount/WordCount.java
+++ src/main/java/demos/wordcount/WordCount.java
@@ -4,9 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.Charset;
+import java.util.*;
 
 /**
  * Count the words' appearance.
@@ -20,7 +19,7 @@ public class WordCount {
 //    private List<Pair<String/*word*/, Integer/*count*/>> wordCount = new ArrayList<>();
 
     // brute-forced
-    public static void count(List<List<String>> data) {
+    public static Map<String/*word*/, Long/*count*/> count(List<List<String>> data) {
         Map<String, Long> wordCount = new HashMap<>();
         for (List<String> sl : data) {
             for (String s : sl) {
@@ -35,13 +34,28 @@ public class WordCount {
 
         LOG.info("word counting finished: total-words<{}>", wordCount.size());
         // todo: which is better? iterate Map.keySet or Map.entrySet
-        for (String word : wordCount.keySet()) {
-            LOG.info("count result: word<{}>, count<{}>", word, wordCount.get(word));
-        }
+//        for (String word : wordCount.keySet()) {
+//            LOG.info("count result: word<{}>, count<{}>", word, wordCount.get(word));
+//        }
+        return wordCount;
     }
 
     public static void top(Map<String, Long> wordCount, int rank) {
-        wordCount.entrySet().stream().sorted()
+        List<Map.Entry<String, Long>> wordCountList = new ArrayList<>(wordCount.entrySet());
+        wordCountList.sort((o1, o2) -> {
+            if (o1.getValue().equals(o2.getValue()))
+                return 0;
+            else
+                return o1.getValue() > o2.getValue() ? -1 : 1;
+        });
+
+        int rankCnt = 0;
+        for (Map.Entry<String, Long> e : wordCountList) {
+            LOG.info("count result: word<{}>, code<{}>, count<{}>", e.getKey(), e.getKey().getBytes(Charset.defaultCharset()),e.getValue());
+            rankCnt++;
+            if (rankCnt == rank)
+                break;
+        }
     }
 
     public static void main(String[] args) {
@@ -55,6 +69,12 @@ public class WordCount {
 
         List<List<String>> data = dataLoader.getData();
 
-        WordCount.count(data);
+        long begin = System.nanoTime();
+        Map<String, Long> wordCount = WordCount.count(data);
+        long end = System.nanoTime();
+
+        WordCount.top(wordCount, 10);
+
+        LOG.info("word count finished in <{}>ms", (end - begin) / 1000000);
     }
 }
